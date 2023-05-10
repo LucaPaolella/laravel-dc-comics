3b546c883a9b31a02cb20754acaf8575233c923c
Your Name <you@example.com>
2023-05-10 16:32:15 +0200

    milestone 1

---
+++ app/Http/Controllers/ComicsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ComicsModel;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ComicsModel  $comicsModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ComicsModel $comicsModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ComicsModel  $comicsModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ComicsModel $comicsModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ComicsModel  $comicsModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ComicsModel $comicsModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ComicsModel  $comicsModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ComicsModel $comicsModel)
+    {
+        //
+    }
+}
